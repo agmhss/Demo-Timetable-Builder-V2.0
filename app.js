58054c65acba68b4d2c7458007f651a8b659aa08
AGMHSS <agmhsspatteeswaram@gmail.com>
2026-05-27 12:05:15 +0530

    Update and rename app (13).js to app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 const APP_CONFIG = {
     fullName: "GHSS VELAMURITHANPETTAI", 
     shortName: "GHSS VMPT",                                           
-    scriptUrl: "https://script.google.com/macros/s/AKfycbwhoxfOTBzWYD7hJyFP2mI8-cfJS77npL4SyAd6N1Ps2RUw29oIHffGejX_hkAjxmWW4g/exec" 
+    scriptUrl: "" 
 };
 const SCRIPT_URL = APP_CONFIG.scriptUrl;
 
